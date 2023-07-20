281d5e8add5a4cf86e138d8a2c4c236b270febef
AdhamMahmoud98 <adhammahmoud6@gmail.com>
2023-07-20 22:59:44 +0200

    feature ready

---
+++ components/greetings.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+const Greetings = () => {
+  const [name, setName] = useState('');
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={(text) => setName(text)}
+      />
+      <Text>Welcome, {name}!</Text>
+    </View>
+  );
+};
+
+export default Greetings;
